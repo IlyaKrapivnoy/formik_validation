8ad312878a3852c07363e09a7b3b28c61a919211
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-23 18:12:15 +0200

    add grid and useStyles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import Header from './Header';
 import './App.css';
+import { Container, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    formWrapper: {
+        marginTop: theme.spacing(5),
+        marginBottom: theme.spacing(8),
+    },
+}));
 
 function App() {
+    const classes = useStyles();
+
     return (
-        <div className='App'>
-            <Header />
-        </div>
+        <Grid container>
+            <Grid item xs={12}>
+                <Header />
+            </Grid>
+            <Grid item xs={12}>
+                <Container maxWidth='md'>
+                    <div className={classes.formWrapper}>Form starts here</div>
+                </Container>
+            </Grid>
+        </Grid>
     );
 }
 
