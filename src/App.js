5b419a947ddd4a0e3147811d93a213a8a71efec7
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-23 18:17:41 +0200

    initialize Formik

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './Header';
 import './App.css';
 import { Container, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 
 const useStyles = makeStyles((theme) => ({
     formWrapper: {
@@ -9,6 +11,10 @@ const useStyles = makeStyles((theme) => ({
         marginBottom: theme.spacing(8),
     },
 }));
+
+const INITIAL_FORM_STATE = {};
+
+const FORM_VALIDATION = Yup.object().shape({});
 
 function App() {
     const classes = useStyles();
@@ -20,7 +26,17 @@ function App() {
             </Grid>
             <Grid item xs={12}>
                 <Container maxWidth='md'>
-                    <div className={classes.formWrapper}>Form starts here</div>
+                    <div className={classes.formWrapper}>
+                        <Formik
+                            initialValues={{ ...INITIAL_FORM_STATE }}
+                            validationSchema={FORM_VALIDATION}
+                            onSubmit={(values) => {
+                                console.log(values);
+                            }}
+                        >
+                            <Form></Form>
+                        </Formik>
+                    </div>
                 </Container>
             </Grid>
         </Grid>
